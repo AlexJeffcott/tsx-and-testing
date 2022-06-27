3172834259e98212413ccbd408ebcef085e96e02
Alex <alexjeffcott@gmail.com>
2022-06-27 11:59:30 +0000

    wip: formating

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [react()],
-    css: { modules: { localsConvention: 'camelCaseOnly' } },
-    server: { hmr: { clientPort: process.env.CODESPACES ? 443 : undefined } }
+	plugins: [react()],
+	css: { modules: { localsConvention: 'camelCaseOnly' } },
+	server: { hmr: { clientPort: process.env.CODESPACES ? 443 : undefined } }
 });
